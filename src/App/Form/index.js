a90208f1c9d493cbde0e4ca2db12c4e87e32cf08
Bartosz Koza <kozabartosz01@gmail.com>
2024-07-16 14:25:11 +0200

    simple currency calculator to react js

---
+++ src/App/Form/index.js
@@ -0,0 +1,63 @@
+import {useState} from "react";
+import { currencies } from "../currencies";
+import { Result } from "./Result";
+import "./style.css";
+
+export const Form = ({ calculateResult, result }) => {
+  const [currency, setCurrency] = useState(currencies[0].short);
+  const [amount, setAmount] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  };
+
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      <h1 className="formHeader">Przelicznik walut</h1>
+
+      <p>
+        <label className="label">
+          <span className="formLabelText">Kwota w PLN</span>
+          <input
+            value={amount}
+            onChange={({ target }) => setAmount(target.value)}
+            placeholder="Wpisz kwotę w zł"
+            className="formField"
+            type="number"
+            step="0.01"
+            min="0"
+            required
+          />
+        </label>
+      </p>
+
+      <p>
+        <label className="label">
+          <span className="formLabelText">Wybierz walutę</span>
+          <select
+            className="formField"
+            value={currency}
+            onChange={({ target }) => setCurrency(target.value)}
+          >
+            {currencies.map((currency) => (
+              <option key={currency.short} value={currency.short}>
+                {currency.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </p>
+
+      <p>
+        <button className="button">Oblicz</button>
+      </p>
+
+      <p className="formInfo">
+        Kursy pochodzą ze strony nbp.pl z tabeli nr 136/A/NBP/2024 z dnia
+        2024-07-15
+      </p>
+      <Result result={result} />
+    </form>
+  );
+};
---
+++ src/App/currencies.js
@@ -0,0 +1,28 @@
+export const currencies = [
+    {
+        short:"AUD",
+        name:"Dolar australijski",
+        rate: 2.6434,
+    },
+    {
+        short:"CAD",
+        name:"Dolar kanadysjki",
+        rate: 2.8576,
+    },
+    {
+        short:"CZK",
+        name:"Czeska korona",
+        rate: 0.1675,
+    },
+    {
+        short:"GBP",
+        name:"Funt szterling",
+        rate: 5.0603,
+    },
+    
+    {
+        short:"USD",
+        name:"Dolar amerykański",
+        rate: 3.8960,
+    }
+]
